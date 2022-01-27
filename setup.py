5c55ca28b02c26c09805f101dc4baeac69bef048
sartaajkhan <67485363+sartaajkhan@users.noreply.github.com>
2022-01-27 02:37:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+#with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+#    long_description = "\n" + fh.read()
 
 VERSION = '0.0.1'
 DESCRIPTION = 'Computes details of solution for modeling purposes'
